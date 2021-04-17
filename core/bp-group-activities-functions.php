997cfbb7268ac3a5a294ebc8c4ee530a16dc6d91
Dernerd <dern3rd@outlook.at>
2021-04-17 03:23:42 +0200

    first commit

---
+++ core/bp-group-activities-functions.php
@@ -0,0 +1,58 @@
+<?php
+// Do not allow direct access over web.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Just formats the notification
+ *
+ * @param string $action action.
+ * @param int    $item_id item id(group id).
+ * @param int    $secondary_item_id activity id.
+ * @param int    $total_items total items.
+ * @param string $format response format.
+ *
+ * @return string|array
+ */
+function bp_local_group_notifier_format_notifications( $action, $item_id, $secondary_item_id, $total_items, $format = 'string' ) {
+
+	$group_id = $item_id;
+	$group = groups_get_group( array( 'group_id' => $group_id ) );
+	$group_link = bp_get_group_permalink( $group );
+
+	if ( (int) $total_items > 1 ) {
+
+		$text = sprintf( __( '%1$d neue Aktivitäten in der Gruppe "%2$s"', 'bp-group-activities-notifier' ), (int) $total_items, $group->name );
+
+		if ( 'string' == $format ) {
+			return '<a href="' . $group_link . '" title="' . __( 'Neue Gruppenaktivitäten', 'bp-group-activities-notifier' ) . '">' . $text . '</a>';
+		} else {
+			return array(
+				'link' => $group_link,
+				'text' => $text,
+			);
+		}
+	} else {
+
+		$activity = new BP_Activity_Activity( $secondary_item_id );
+
+		$text = strip_tags( $activity->action );//here is the hack, think about it :)
+
+		$notification_link = apply_filters(
+			'bp_local_group_notifier_notification_activity_permalink',
+			bp_activity_get_permalink( $activity->id, $activity ),
+			$item_id,
+			$secondary_item_id,
+			$total_items,
+			$format
+		);
+
+		if ( 'string' == $format ) {
+			return '<a href="' . $notification_link . '" title="' . $text . '">' . $text . '</a>';
+		} else {
+			return array(
+				'link' => $notification_link,
+				'text' => $text,
+			);
+		}
+	}
+}
---
+++ loader.php
@@ -0,0 +1,63 @@
+<?php
+// Do not allow direct access over web.
+defined( 'ABSPATH' ) || exit;
+
+
+/**
+ * Dummy Component.
+ *
+ * We need a dummy component for manipulating notifications.
+ */
+class BPLocalGroupNotifier extends BP_Component {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+
+		$bp = buddypress();
+		parent::start(
+			'localgroupnotifier',
+			__( 'Lokaler Gruppenbenachrichtiger', 'bp-local-group-notifier' ),
+			plugin_dir_path( __FILE__ )
+		);
+
+		$bp->active_components[ $this->id ] = 1;
+	}
+
+
+	public function includes( $files = array() ) {
+	}
+
+	/**
+	 * Setup globals.
+	 *
+	 * @param array $global globals.
+	 */
+	public function setup_globals( $global = array() ) {
+
+		// All globals for messaging component.
+		// Note that global_tables is included in this array.
+		$globals = array(
+			'slug'                  => $this->id,
+			'root_slug'             => false,
+			'has_directory'         => false,
+			'notification_callback' => 'bp_local_group_notifier_format_notifications',
+			// Bp currently does not support object method callbacks here.
+			'global_tables'         => false,
+		);
+
+		parent::setup_globals( $globals );
+
+	}
+
+}
+
+/**
+ * Registerr Dummy Component.
+ */
+function bp_setup_local_group_notifier() {
+	buddypress()->localgroupnotifier = new BPLocalGroupNotifier();
+}
+
+add_action( 'bp_loaded', 'bp_setup_local_group_notifier' );
